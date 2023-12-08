adb38e3c26f6eac8d56a8c2360ccccfed57e7045
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-12-08 08:36:14 +0300

    Remove unused dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,7 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.camera:camera-core:1.3.0")
 }
