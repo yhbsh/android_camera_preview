944012cf58ea4758d27291d34ce3160925a8ec72
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-12-08 09:13:33 +0300

    Refactor camera preview logic to a separate class and add frame layout

---
+++ app/src/main/java/com/example/camera_viewer/CameraPreview.java
@@ -0,0 +1,72 @@
+package com.example.camera_viewer;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+
+/**
+ * @noinspection ALL
+ */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private Camera camera;
+
+    private SurfaceHolder surfaceHolder;
+
+    private int cameraId = 0;
+
+    public CameraPreview(Context context, int camerId) {
+        super(context);
+        this.cameraId = cameraId;
+        surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+    }
+
+    public void switchCamera(int cameraId) {
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
+        this.cameraId = cameraId;
+        openCamera();
+    }
+
+    private void openCamera() {
+        camera = Camera.open(cameraId);
+        camera.setDisplayOrientation(90);
+        startCameraPreview();
+    }
+
+    private void startCameraPreview() {
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+        openCamera();
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+        if (camera != null) {
+            camera.toString();
+            camera.release();
+            camera = null;
+        }
+    }
+}
--- app/src/main/java/com/example/camera_viewer/MainActivity.java
+++ app/src/main/java/com/example/camera_viewer/MainActivity.java
@@ -1,25 +1,17 @@
 package com.example.camera_viewer;
 
-import android.hardware.Camera;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.io.IOException;
 
 /**
  * @noinspection ALL
  */
-public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
-    static final Integer CAMERA_PERMISSION_CODE = 200;
-    private Camera camera;
-    private SurfaceHolder surfaceHolder;
-
+public class MainActivity extends AppCompatActivity {
+    private CameraPreview cameraPreview;
     private int cameraId = 0;
 
     @Override
@@ -27,31 +19,17 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final SurfaceView surfaceView = findViewById(R.id.surfaceView);
-        surfaceHolder = surfaceView.getHolder();
-        surfaceHolder.addCallback(this);
+        cameraPreview = new CameraPreview(this, cameraId);
+        FrameLayout preview = findViewById(R.id.camera_preview);
+        preview.addView(cameraPreview);
 
-        final Button button = findViewById(R.id.btn_switch_camera);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        Button switchButton = findViewById(R.id.btn_switch_camera);
+        switchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (cameraId == 0) {
-                    cameraId = 1;
-                } else {
-                    cameraId = 0;
-                }
-
-                camera = Camera.open(cameraId);
-
-                camera.setDisplayOrientation(90);
-
-                try {
-                    camera.setPreviewDisplay(surfaceHolder);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-
-                camera.startPreview();
+                cameraId = (cameraId == 0) ? 1 : 0;
+                cameraPreview.switchCamera(cameraId);
             }
         });
     }
@@ -60,45 +38,10 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     @Override
     protected void onPause() {
         super.onPause();
-        if (camera != null) {
-            camera.stopPreview();
-        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (camera != null) {
-            camera.startPreview();
-        }
-    }
-
-    @Override
-    public void surfaceCreated(@NonNull SurfaceHolder holder) {
-        camera = Camera.open(cameraId);
-
-        camera.setDisplayOrientation(90);
-
-        try {
-            camera.setPreviewDisplay(holder);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        camera.startPreview();
-    }
-
-    @Override
-    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
-        if (camera != null) {
-            camera.toString();
-            camera.release();
-            camera = null;
-        }
     }
 }
