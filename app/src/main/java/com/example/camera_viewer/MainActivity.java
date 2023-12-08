e4bdb7d8bf6b6200f1e44e0ab5d1f1873b41f3c0
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-12-08 23:16:56 +0300

    Remove switch camera button and simplify UI

--- app/src/main/java/com/example/camera_viewer/MainActivity.java
+++ app/src/main/java/com/example/camera_viewer/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.camera_viewer;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,14 +22,5 @@ public class MainActivity extends AppCompatActivity {
 
         FrameLayout preview = findViewById(R.id.camera_preview);
         preview.addView(mPreview);
-
-        Button switchButton = findViewById(R.id.btn_switch_camera);
-        switchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cameraId = (cameraId == 0) ? 1 : 0;
-                mPreview.switchCamera(cameraId);
-            }
-        });
     }
 }
