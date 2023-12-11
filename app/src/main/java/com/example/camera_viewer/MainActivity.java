de6f583577b25b93a471617f81306e133d40a991
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-12-11 15:43:18 +0300

    Simplify logic

--- app/src/main/java/com/example/camera_viewer/MainActivity.java
+++ app/src/main/java/com/example/camera_viewer/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.camera_viewer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.FrameLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.IOException;
 
 /**
  * @noinspection ALL
  */
-public class MainActivity extends AppCompatActivity {
-    private CameraPreview mPreview;
-    private int cameraId = 1;
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+    Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +27,38 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        mPreview = new CameraPreview(this, cameraId);
+        while (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 200);
+        }
 
         FrameLayout cameraPreview = findViewById(R.id.camera_preview);
-        cameraPreview.addView(mPreview);
+        SurfaceView surfaceView = new SurfaceView(this);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        cameraPreview.addView(surfaceView);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+        try {
+            camera = Camera.open(1);
+            camera.setDisplayOrientation(90);
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
     }
 }
